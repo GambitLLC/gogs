383f10b4a52eabaab9f919dad220fbdf1a730bdf
Randolph Xia <xiarandolph@gmail.com>
2021-02-21 17:04:13 -0500

    Fix player position calculation

--- impl/net/handlers/player_position_handler.go
+++ impl/net/handlers/player_position_handler.go
@@ -18,13 +18,12 @@ func PlayerPosition(c gnet.Conn, pkt *pk.Packet, s api.Server) error {
 		return err
 	}
 
-	// TODO: why doesn't this work
 	// send new player position to everyone else
 	playerPositionPacket := clientbound.EntityPosition{
 		EntityID: pk.VarInt(player.EntityID),
-		DeltaX:   pk.Short(float64(pos.X) - player.Position.X),
-		DeltaY:   pk.Short(float64(pos.Y) - player.Position.Y),
-		DeltaZ:   pk.Short(float64(pos.Z) - player.Position.Z),
+		DeltaX:   pk.Short(float64(pos.X*32)-player.Position.X*32) * 128,
+		DeltaY:   pk.Short(float64(pos.Y*32)-player.Position.Y*32) * 128,
+		DeltaZ:   pk.Short(float64(pos.Z*32)-player.Position.Z*32) * 128,
 		OnGround: pos.OnGround,
 	}.CreatePacket().Encode()
 	for _, p := range s.Players() {
