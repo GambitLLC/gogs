f77c66985935040a70f7cceae05fd0d9674a1376
Randolph Xia <xiarandolph@gmail.com>
2021-02-21 19:10:47 -0500

    Remove unused code

--- impl/net/handlers/player_position_and_rotation_handler.go
+++ impl/net/handlers/player_position_and_rotation_handler.go
@@ -30,28 +30,5 @@ func PlayerPositionAndRotation(c gnet.Conn, pkt *pk.Packet, s api.Server) error 
 		OnGround: bool(in.OnGround),
 	})
 
-	//outPacket := clientbound.EntityPositionAndRotation{
-	//	EntityID: pk.VarInt(player.GetEntityID()),
-	//	DeltaX:   pk.Short(float64(data.X*32)-player.GetPosition().X*32) * 128,
-	//	DeltaY:   pk.Short(float64(data.Y*32)-player.GetPosition().Y*32) * 128,
-	//	DeltaZ:   pk.Short(float64(data.Z*32)-player.GetPosition().Z*32) * 128,
-	//	Yaw:      pk.Angle(data.Yaw / 360 * 256),
-	//	Pitch:    pk.Angle(data.Pitch / 360 * 256),
-	//	OnGround: data.OnGround,
-	//}.CreatePacket().Encode()
-	//
-	//// also send head rotation packet
-	//outPacket = append(outPacket, clientbound.EntityHeadLook{
-	//	EntityID: pk.VarInt(player.GetEntityID()),
-	//	HeadYaw:  pk.Angle(data.Yaw / 360 * 256),
-	//}.CreatePacket().Encode()...)
-	//
-	//for _, p := range s.Players() {
-	//	conn := s.ConnFromUUID(p.GetUUID())
-	//	if conn != c {
-	//		_ = conn.AsyncWrite(outPacket)
-	//	}
-	//}
-
 	return nil
 }
