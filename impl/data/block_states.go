a60e74026df953851ebe1f45b82d838986acb48e
Randolph Xia <xiarandolph@gmail.com>
2021-03-20 15:58:53 -0400

    Make BlockStateID store all block states rather than opening the file multiple times

--- impl/data/block_states.go
+++ impl/data/block_states.go
@@ -15,39 +15,53 @@ func BlockStateID(name string, properties map[string]interface{}) int32 {
 		blocksMap = make(map[string]map[string]interface{})
 		// Open our jsonFile
 		jsonFile, _ := os.Open("./data-generator/generated/reports/blocks.json")
+		defer jsonFile.Close()
 		byteValue, _ := ioutil.ReadAll(jsonFile)
 
-		_ = json.Unmarshal(byteValue, &blocksMap)
-		jsonFile.Close()
+		err := json.Unmarshal(byteValue, &blocksMap)
+		if err != nil {
+			return 0
+		}
+
+		// load all blocks into idMap
+		for name, blockMap := range blocksMap {
+			if idMap[name] == nil {
+				idMap[name] = make(map[string]int32)
+			}
+
+			for _, state := range blockMap["states"].([]interface{}) {
+				stateObj := state.(map[string]interface{})
+				properties, exists := stateObj["properties"].(map[string]interface{})
+				id := int32(stateObj["id"].(float64))
+				if exists {
+					obj, err := json.Marshal(properties)
+					if err != nil {
+						continue
+					}
+					idMap[name][string(obj)] = id
+				}
+
+				if isDefault, exists := stateObj["default"].(bool); exists && isDefault {
+					idMap[name]["default"] = id
+				}
+			}
+		}
 	}
 
 	if idMap[name] == nil {
-		idMap[name] = make(map[string]int32)
+		logger.Printf("block state doesn't exist: %s", name)
+		return 0
+	}
+
+	if properties == nil {
+		return idMap[name]["default"]
 	}
 
 	obj, _ := json.Marshal(properties)
 	val, exists := idMap[name][string(obj)]
 	if !exists {
-		block, exists := blocksMap[name]
-		if !exists {
-			logger.Printf("block state doesn't exist: %v", name)
-			return 0
-		}
-		index := int64(0)
-		for k, v := range properties {
-			index *= 2
-			propertyVals := block["properties"].(map[string]interface{})[k].([]interface{})
-			for i, val := range propertyVals {
-				if v == val {
-					index += int64(i)
-					break
-				}
-			}
-		}
-
-		id := int32(block["states"].([]interface{})[index].(map[string]interface{})["id"].(float64))
-		idMap[name][string(obj)] = id
-		return id
+		logger.Printf("block state %s with properties %v doesn't exist", name, properties)
+		return 0
 	}
 
 	return val
